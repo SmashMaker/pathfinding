3dca42d7315eede16b369380192ffeb83688a378
SmashMaker <paullegall0502@gmail.com>
2024-02-10 16:38:42 +0100

    Modification du BFS Rend l'algorithme BFS utilisable avec l'appel de la fonction solve() Retourne le path

--- BFS.py
+++ BFS.py
@@ -1,18 +1,4 @@
 import Grid
-import random
-
-import time
-import test_maze
-possible_cases = {
-    "Indescructible wall": -1,
-    "empty cell": 0,
-    "wall": 1,
-    "start": 2,
-    "end": 3,
-    "visited cell": 4,
-    "path cell": 5,
-    "current cell": 6
-}
 
 def get_neighbors_informations(maze, current, useless_neighbors):
     # Get raw neighbors
@@ -32,68 +18,77 @@ def get_neighbors_informations(maze, current, useless_neighbors):
     # [x, y, case_type]
     return neighbors
 
-maze = Grid.Grid(test_maze.normal_mazes["normal_maze_1"]["grid"], possible_cases)
-start = maze.findCase("start")
-end = maze.findCase("end")
+def solve(gridobjoriginal: Grid, step_by_step=False):
 
-current = start
-paths = [[current]]
+    gridobj = gridobjoriginal.copy()
 
-# Get useless neighbors (get their number)
-useless_neighbors = ["Indescructible wall", "visited cell", "path cell", "wall", "current cell","start"]
-for key in useless_neighbors:
-    useless_neighbors[useless_neighbors.index(key)] = maze.possible_cases[key]
+    start = gridobj.findCase("start")
+    end = gridobj.findCase("end")
 
-destroy_list = []
+    current = start
+    paths = [[current]]
 
-is_searching = True
-while is_searching:
-    maze.printGrid()
-    if destroy_list:
-        for i in sorted(destroy_list, reverse=True):
-            paths.pop(i)
-        destroy_list = []
+    # Get useless neighbors (get their number)
+    useless_neighbors = ["Indescructible wall", "visited cell", "path cell", "wall", "current cell", "start"]
+    for key in useless_neighbors:
+        useless_neighbors[useless_neighbors.index(key)] = gridobj.possible_cases[key]
 
-    new_paths = []
-    for index_path, path in enumerate(paths):
-        current = path[-1]
-        # Avant de chercher de nouveaux voisins, marquez la cellule courante comme visitée s'il s'agit pas de la cellule de départ
-        if current != start:
-            maze.setCase(current[0], current[1], "visited cell")
+    destroy_list = []
 
-        possible_way = get_neighbors_informations(maze, current, useless_neighbors)
+    is_searching = True
+    while is_searching:
+        # Destroy every path where there is no way
+        if destroy_list:
+            for i in sorted(destroy_list, reverse=True):
+                paths.pop(i)
+            destroy_list = []
 
-        # Check if end
-        for neighbor in possible_way:
-            if neighbor[2] == maze.possible_cases["end"]:
-                is_searching = False
-                path.append((neighbor[0], neighbor[1]))
-                print("Path to the end found!")
+        # Check every paths
+        new_paths = []
+        for index_path, path in enumerate(paths):
+            current = path[-1]
+            if current != start:
+                gridobj.setCase(current[0], current[1], "visited cell")
+
+            possible_way = get_neighbors_informations(gridobj, current, useless_neighbors)
+
+            # Check if end
+            for neighbor in possible_way:
+                if neighbor[2] == gridobj.possible_cases["end"]:
+                    is_searching = False
+                    path.append((neighbor[0], neighbor[1]))
+                    return path
+
+            if not is_searching:
                 break
 
-        if not is_searching:
-            break
+            # Act depending on the number of possible ways
+            nb_possible_way = len(possible_way)
 
-        nb_possible_way = len(possible_way)
-        if nb_possible_way == 0:
-            destroy_list.append(index_path)
-        elif nb_possible_way >= 1:
-            for i, way in enumerate(possible_way):
-                new_current = (way[0], way[1])
-                if i == 0:  # Continue the original path
-                    path.append(new_current)
-                    # Marquez seulement la nouvelle cellule courante
-                    maze.setCase(new_current[0], new_current[1], "current cell")
-                else:  # Créez de nouveaux chemins pour les autres possibilités
-                    new_path = list(
-                        path[:-1])  # Excluez la cellule courante actuelle qui est déjà marquée comme visitée
-                    new_path.append(new_current)
-                    new_paths.append(new_path)
-                    maze.setCase(new_current[0], new_current[1], "current cell")
+            # If there is no way, we add to the destroy list
+            if nb_possible_way == 0:
+                destroy_list.append(index_path)
 
-    paths.extend(new_paths)  # Ajoutez les nouveaux chemins à la liste principale des chemins
+            elif nb_possible_way >= 1:
+                for i, way in enumerate(possible_way):
+                    new_current = (way[0], way[1])
 
-    # Break if there are no paths left to explore
-    if not paths:
-        print("No path to the end exists.")
-        break
+                    # If there is one way, we take a step
+                    if i == 0:  # Continue the original path
+                        path.append(new_current)
+                        gridobj.setCase(new_current[0], new_current[1], "current cell")
+
+                    # If there is more than one, we split for every way and take a step
+                    else:  # Créez de nouveaux chemins pour les autres possibilités
+                        # Add the new path (split)
+                        new_path = list(
+                            path[:-1])
+                        new_path.append(new_current)
+                        new_paths.append(new_path)
+                        gridobj.setCase(new_current[0], new_current[1], "current cell")
+
+        paths.extend(new_paths)
+
+        # Break if there are no paths left to explore
+        if not paths:
+            return None
